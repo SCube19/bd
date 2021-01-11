751fbf9f89a1512091576c7d0cd4de9b64a3a6db
Konrad Korczyński <kk418331@students.mimuw.edu.pl>
2021-01-11 19:46:57 +0100

    :DD

--- warcaby.php
+++ warcaby.php
@@ -10,27 +10,24 @@
   <meta name="author" content="SitePoint">
 
   <link rel="stylesheet" href="styles.css">
+
   <link rel="shortcut icon" href="https://www.mimuw.edu.pl/sites/default/files/mim_mini.png" type="image/png">
 </head>
 
 <body>
-
+    <h1>Warcaby</h1>
   <?php if (isset($_COOKIE['active_username'])) : ?>
-    <div class="center"><a href="profile.php">PROFIL</a></div>
     <div class="center"><a href="logout.php">WYLOGUJ</a></div>
+    <img class="parowa" src="https://upload.wikimedia.org/wikipedia/commons/f/fb/Hotdog_-_Evan_Swigart.jpg">
   <?php else : ?>
     <div class="center">
       <a href="login_page.php">LOGOWANIE</a><br><br>
       <a href="registration_page.php">REJESTRACJA</a>
     </div>
+    <img class="parowa" src="https://s3.amazonaws.com/rapgenius/hotdog.jpg">
   <?php endif; ?>
-
-  <a href="szachy.php">Szachy</a><br><br>
-  <a href="bierki.php">Bierki</a><br><br>
-  <a href="warcaby.php">Warcaby</a><br><br>
-  <a href="poker.php">Poker</a><br><br>
-  <a href="pilka.php">Piłka</a><br><br>
-  <a href="leaderboards.php">Rankingi</a><br><br>
+  <a href='index.php'>Strona główna</a>
+  <a href='leaderboards.php'>Rankingi</a>
 
 </body>
 
